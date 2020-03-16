68fcc6db7465fce4973e54786a6a270dcf156def
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-03-17 01:11:51 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let instr = String(Math.abs(n));
+    let newString = '';
+    for (let i = instr.length - 1; i >= 0; i--) {
+        newString += instr[i];
+    }
+    return parseInt(newString);
 }
